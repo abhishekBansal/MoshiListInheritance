becf0f467d4ad72b4291a720f0d47e63d5421f61
Abhishek Bansal <discover.ab@gmail.com>
2021-07-05 16:41:19 +0530

    make things even simpler

--- app/src/main/java/dev/abhishekbansal/moshilistinheritance/Question.kt
+++ app/src/main/java/dev/abhishekbansal/moshilistinheritance/Question.kt
@@ -3,7 +3,7 @@ package dev.abhishekbansal.moshilistinheritance
 import com.squareup.moshi.JsonClass
 import java.util.*
 
-sealed class QuestionV2(
+open class QuestionV2(
     val type: String,
     val id: String,
     val options: List<OptionV2>?
@@ -12,17 +12,10 @@ sealed class QuestionV2(
 }
 
 @JsonClass(generateAdapter = true)
-open class SelectionQuestion(
-    type: String,
-    id: String,
-    options: List<SelectionOption>?,
-) : QuestionV2(type, id, options)
-
-@JsonClass(generateAdapter = true)
 class BubbleQuestion(
     type: String,
     id: String,
     options: List<SelectionOption>?,
 
     val select: List<String>?
-) : SelectionQuestion(type, id, options)
+) : QuestionV2(type, id, options)
